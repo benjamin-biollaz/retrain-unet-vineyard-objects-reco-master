8843c7fce293ad280c43c97c5b6a78617a244e99
benjamin <benjamin.biollaz@students.hevs.ch>
2023-06-13 21:25:52 +0200

    removed unused code

--- unet_model.py
+++ unet_model.py
@@ -30,39 +30,19 @@ def unet_sym(pretrained_weights=None, input_size=(144, 144, 3)):
     merge8 = concatenate([conv2, up8], axis=3)
     up9 = UpSampling2D(size=(3, 3))(merge8)
     up10 = UpSampling2D(size=(2, 2))(up9)
+    
                 #filters #kernel
     conv10 = Conv2D(3, (1, 1), activation='softmax', padding='same', kernel_initializer='he_normal')(up10)
 
     model = Model(inputs, conv10)
 
-    #model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
     model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
-
 
     if pretrained_weights:
       model.load_weights(pretrained_weights, by_name=True, skip_mismatch=True)
-        #load_weights_by_name(model, pretrained_weights, True)
 
     return model
 
-def load_weights_by_name(model, path, verbose=False):
-    def load_model_weights(cmodel, weights):
-        for layer in cmodel.layers:
-            print(layer.name)
-            if hasattr(layer, 'layers'):
-                load_model_weights(layer, weights[layer.name])
-            else:
-                for w in layer.weights:
-                    _, name = w.name.split('/')
-                    if verbose:
-                        print(w.name)
-                    try:
-                        w.assign(weights[layer.name][name][()])
-                    except:
-                        w.assign(weights[layer.name][layer.name][name][()])
-
-    with h5py.File(path, 'r') as f:
-        load_model_weights(model, f)
     
 
 
